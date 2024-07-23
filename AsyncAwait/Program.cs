1c712b261c781b0c48aa9725e071b98d3eb05323
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-07-23 15:07:48 +0200

    Modul 008 Async Await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace AsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Einfaches Beispiel: Zwei Dateien herunterladen
+            //SequentialSample();
+
+            // Async: Zwei Dateien parallel herunterladen
+            await ParallelAsyncSample();
+        }
+
+        private static void SequentialSample()
+        {
+            Task.Delay(10000).Wait();
+            Console.WriteLine("Download file 1");
+
+            Task.Delay(10000).Wait();
+            Console.WriteLine("Download file 2");
+
+            Console.WriteLine("Start data input: Enter name");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}");
+
+            Console.ReadLine();
+        }
+
+        private static async Task ParallelAsyncSample()
+        {
+            // An dieser Stelle werden zwangslauefig keine neuen Threads angelegt sondern sog. Coroutines verwendet
+            Download1();
+
+            Download2();
+
+            Console.WriteLine("Start data input: Enter name");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}");
+
+            Console.ReadLine();
+        }
+
+        private static async Task Download2()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Download file 2 -- Thread ID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static async Task Download1()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Download file 1 -- Thread ID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ AsyncAwaitWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static int GetRandomTimespan()
+        {
+            return (Random.Shared.Next() % 2000) + 1000;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Text = "";
+
+            var list = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(Task.Run(() => Thread.Sleep(GetRandomTimespan())));
+            }
+            Task.WaitAll([.. list]); // Blockierende Operation
+            Info.Text = "Tasks fertig";
+        }
+
+        private async void StartAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Text = "";
+
+            var list = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(Task.Delay(GetRandomTimespan()));
+            }
+            await Task.WhenAll([.. list]); // Blockiert nicht
+            Info.Text = "Tasks fertig";
+        }
+
+        private async void RequestAsync_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+                var result = await response.Content.ReadAsStringAsync();
+                Info.Text = result;
+            }
+        }
+    }
+}
