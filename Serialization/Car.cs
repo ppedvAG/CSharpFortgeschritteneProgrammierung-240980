dd6ed515724eab6c38b9491ebf5dd1b43eed9c0e
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-07-23 15:49:00 +0200

    Modul 005 Serialisierung

---
+++ Serialization/Car.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Serialization.Models
+{
+    public class Car
+    {
+        [XmlIgnore]
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        [XmlAttribute]
+        public string Type { get; set; }
+
+        [XmlAttribute]
+        [JsonPropertyName("fuelType")]
+        public string Fuel { get; set; }
+
+        public int TopSpeed { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))] // wandelt Enum-Werte in Strings um 
+        public KnownColor Color { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Manufacturer} - {Model} - {Type} - {Fuel} - TopSpeed: {TopSpeed} - Color: {Color}";
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,104 @@
+﻿using Bogus.DataSets;
+using Bogus;
+using System.Drawing;
+using Serialization.Models;
+using System.Xml.Serialization;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Bogus.Bson;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        public static readonly Faker _faker = new Faker();
+        public const int ReservedSystemColorNameCount = 27;
+        public static readonly int ColorCount = Enum.GetValues(typeof(KnownColor)).Length;
+
+        static async Task Main(string[] args)
+        {
+            var carList = Enumerable.Range(0, 10).Select(GenerateVehicle).ToArray();
+
+            //XmlSample(carList, "cars.xml");
+
+            Console.WriteLine("\n\nSample System.Text.Json");
+            JsonSample(carList);
+
+            Console.WriteLine("\n\nSample Newtonsoft.Json");
+            NewtonsoftJsonSample(carList);
+        }
+
+        private static void XmlSample<T>(T[] carList, string fileName) where T : class
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]));
+
+            using (var writer = new StreamWriter(fileName))
+            {
+                serializer.Serialize(writer, carList);
+            }
+
+            using (var reader = new StreamReader(fileName))
+            {
+                var result = serializer.Deserialize(reader) as T[];
+
+                foreach (var car in result)
+                {
+                    Console.WriteLine(car.ToString());
+                }
+            }
+        }
+
+        private static async Task JsonSample<T>(T[] carList)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var json = System.Text.Json.JsonSerializer.Serialize(carList, options);
+            await File.WriteAllTextAsync("cars.json", json);
+
+            var fileContent = await File.ReadAllTextAsync("cars.json");
+            var result = System.Text.Json.JsonSerializer.Deserialize<T[]>(fileContent);
+            foreach (var car in result)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+
+        private static async Task NewtonsoftJsonSample<T>(T[] carList)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects // Vererbung ermoeglichen
+            };
+
+            var json = JsonConvert.SerializeObject(carList, settings);
+            await File.WriteAllTextAsync("cars_newtonsoft.json", json);
+
+            var fileContent = await File.ReadAllTextAsync("cars_newtonsoft.json");
+            var result = JsonConvert.DeserializeObject<T[]>(fileContent);
+            foreach (var car in result)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+
+        public static Car GenerateVehicle(int id)
+        {
+            var topSpeed = Random.Shared.Next(10, 25) * 10;
+            var color = (KnownColor)Random.Shared.Next(ReservedSystemColorNameCount, ColorCount);
+            return new Car
+            {
+                Id = id,
+                Manufacturer = _faker.Vehicle.Manufacturer(),
+                Model = _faker.Vehicle.Model(),
+                Type = _faker.Vehicle.Type(),
+                Fuel = _faker.Vehicle.Fuel(),
+                TopSpeed = topSpeed,
+                Color = color
+            };
+        }
+    }
+}
