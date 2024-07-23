9852d18ffffaf351bf4984cc367208b8ea34b0eb
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-07-23 13:12:10 +0200

    Lab Images Loesung

--- Lab_Imageviewer/Scanner.cs
+++ Lab_Imageviewer/Scanner.cs
@@ -1,15 +1,38 @@
-﻿namespace Lab_Imageviewer;
+﻿using System.Collections.Concurrent;
 
-/// <summary>
-/// Die Scanner Klasse soll kontinuierlich einen Ordner überprüfen, ob neue Images zur Verarbeitung aufgetaucht sind.
-/// Dafür soll diese Klasse intern einen Task besitzen, der diese Arbeit übernimmt.
-/// Es soll auch sicher gestellt werden, das bereits gescannte/verarbeitete Images nicht doppelt gescannt/verarbeitet werden.
-/// Der Benutzer soll die Möglichkeit haben, mehrere Scanner zu Erstellen und dadurch mehrere Ordner gleichzeitig zu Verarbeiten.
-/// </summary>
-public class Scanner
+namespace Lab_Images;
+
+public class Scanner : Runnable
 {
-    public Scanner()
+    public static readonly ConcurrentBag<string> ProcessedImages = new();
+    public static readonly ConcurrentQueue<string> ImagePathQueue = new();
+
+    public string ScanPath { get; private set; }
+
+    public Scanner(string path)
     {
-        
+        ScanPath = path;
+		CurrentTask = new Task(Run);
     }
+
+	protected private override void Run()
+	{
+		while (Continue)
+		{
+			string[] pfade = Directory.GetFiles(ScanPath);
+			foreach (string s in pfade)
+			{
+				if (ProcessedImages.Contains(s) || ImagePathQueue.Contains(s))
+					continue;
+				ImagePathQueue.Enqueue(s);
+			}
+
+			//Directory.GetFiles(ScanPath)
+			//	.Where(e => !ProcessedImages.Contains(e) && !ImagePathQueue.Contains(e))
+			//	.ToList()
+			//	.ForEach(e => ImagePathQueue.Enqueue(e));
+
+			Thread.Sleep(1000);
+		}
+	}
 }
